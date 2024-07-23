302bb9ea583d031cfb5de88d83335955bb2ac5a1
lingfliu <lingfeng.liu@163.com>
2024-07-23 23:52:43 +0800

    UPDATE srv

--- mock/tcp_srv/tcp_srv.go
+++ mock/tcp_srv/tcp_srv.go
@@ -7,14 +7,25 @@ import (
 
 	"lingfliu.github.com/ucs_comm_test/conn"
 	"lingfliu.github.com/ucs_comm_test/ulog"
+	"lingfliu.github.com/ucs_comm_test/utils"
 )
 
-func _task_handle_recv(rx chan []byte, tx chan []byte) {
-	for {
+func _task_handle_recv(rx chan []byte, tx chan []byte, c *conn.TcpConn) {
+	for c.Stat >= 0 {
 		select {
 		case rx_buff := <-rx:
+			c.LastRecvAt = utils.CurrentTimeInMilli()
 			tx <- rx_buff
 		}
+	}
+}
+
+func _task_handle_conn(c *conn.TcpConn) {
+	time.Sleep(1 * time.Second)
+	tic := utils.CurrentTimeInMilli()
+	if tic-c.LastRecvAt > 1000*1000*1000*2 {
+		c.Stat = -1
+		c.Close()
 	}
 }
 
@@ -26,18 +37,22 @@ func main() {
 	}
 	logPath := path.Join(dir, "log.log")
 	ulog.Config(ulog.LOG_LEVEL_INFO, logPath, false)
+	srvConn := conn.NewTcpConn("", 10071)
 
-	conn := conn.NewTcpConn("", 10071)
+	chanC := make(chan *(conn.TcpConn))
+	go srvConn.Accept(chanC)
 
-	conn.Accept()
+	select {
+	case c := <-chanC:
+		c.Stat = 0
+		tx := make(chan []byte)
+		rx := make(chan []byte)
 
-	tx := make(chan []byte)
-	rx := make(chan []byte)
-
-	conn.StartRecv(rx)
-	conn.StartWrite(tx)
-
-	go _task_handle_recv(rx, tx)
+		c.StartRecv(rx)
+		c.StartWrite(tx)
+		go _task_handle_recv(rx, tx, c)
+		go _task_handle_conn(c)
+	}
 
 	for {
 		// ulog.Log().I("test tcp srv test", "sleep")
